40a01415c0ff647424588bfe7dbdde309920159e
SENJ01 <senuvijayasinghe@gmail.com>
2024-03-08 07:12:10 +0530

    Added botApiService.ts

---
+++ src/services/botApiService.ts
@@ -0,0 +1,7 @@
+class botApiService{
+    static getBotResponse(userMessage: string): string{
+        const botResponse: string = "sample response";
+            return botResponse;
+    }
+}
+export default botApiService;
