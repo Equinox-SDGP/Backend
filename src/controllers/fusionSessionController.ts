61ed84bb4daa5e612f14212849e0c3b6156e8c4f
Senuvi Jayasinghe <senuvijayasinghe@gmail.com>
2024-03-30 11:31:11 +0530

    changes

--- src/controllers/fusionSessionController.ts
+++
@@ -1,6 +0,0 @@
-import * as fusionSessionService from "../services/fusionSessionService";
-
-export const setPeriodicFusionSession = async () => {
-  const token = await fusionSessionService.getFusionToken();
-  await fusionSessionService.setFusionToken(token);
-};
--- src/controllers/solarPredictController.ts
+++
@@ -1,5 +0,0 @@
-import * as spaceDataService from '../services/spaceDataService';                                                            
-
-export const getSolarPredictByHour = async () => {
-    const spaceData = await spaceDataService.getSpaceData();
-}
