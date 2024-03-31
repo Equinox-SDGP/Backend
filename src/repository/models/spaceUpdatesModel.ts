1b870af37e89498353530712971e3e061422d0f8
Senuvi Jayasinghe <senuvijayasinghe@gmail.com>
2024-03-31 05:36:40 +0530

    fixed the error in sapceUpdatesModel.ts

--- src/repository/models/spaceUpdatesModel.ts
+++ src/repository/models/spaceUpdatesModel.ts
@@ -6,14 +6,16 @@ export enum UPDATE_INTERVAL {
   MONTH = "month",
   YEAR = "year",
 }
+export interface IDataItemMap { // Now explicitly exported
+  radiation_intensity: string | null;
+  theory_power: number | null;
+  inverter_power: number | null;
+  ongrid_power: number | null;
+  power_profit: number | null;
+}
+
 export interface IUpdateSpace extends Document {
-  dataItemMap: {
-    radiation_intensity: string | null;
-    theory_power: number | null;
-    inverter_power: number | null;
-    ongrid_power: number | null;
-    power_profit: number | null;
-  };
+  dataItemMap: IDataItemMap;
   stationCode: string;
   collectTime: number;
   updateInterval: UPDATE_INTERVAL;
